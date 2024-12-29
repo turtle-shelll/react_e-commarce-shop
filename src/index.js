858e225ee472aedf01fc9a8497117d910e1ebdaa
turtle-shelll <hardik.20022013@gmail.com>
2024-12-29 16:40:22 +0530

    connect to bedrock

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { StoreProvider } from "./storeConfiguration/storeContext";
-// import reportWebVitals from './reportWebVitals';
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import { StoreProvider } from "./storeConfiguration/storeContext";
+// // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <StoreProvider>
+//       <App />
+//     </StoreProvider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+  
+// );
